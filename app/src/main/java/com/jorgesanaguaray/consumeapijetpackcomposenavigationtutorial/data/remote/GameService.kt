847f686963788927be09e844837bda0bdfda09ec
Jorge Sanaguaray <jorgesanaguaray1@gmail.com>
2022-11-18 23:41:25 -0500

    I created GameService in charge of returning a list of games and a game by id from GameApi.

---
+++ app/src/main/java/com/jorgesanaguaray/consumeapijetpackcomposenavigationtutorial/data/remote/GameService.kt
@@ -0,0 +1,33 @@
+package com.jorgesanaguaray.consumeapijetpackcomposenavigationtutorial.data.remote
+
+import com.jorgesanaguaray.consumeapijetpackcomposenavigationtutorial.data.remote.models.GameModel
+import com.jorgesanaguaray.consumeapijetpackcomposenavigationtutorial.data.remote.models.SpecificGameModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+/**
+ * Created by Jorge Sanaguaray
+ */
+
+class GameService @Inject constructor(private val gameApi: GameApi) {
+
+    suspend fun getGames(): List<GameModel> {
+
+        return withContext(Dispatchers.IO) {
+            val games = gameApi.getGames()
+            games.body() ?: emptyList()
+        }
+
+    }
+
+    suspend fun getGameById(id: Int): SpecificGameModel {
+
+        return withContext(Dispatchers.IO) {
+            val game = gameApi.getGameById(id)
+            game.body()!!
+        }
+
+    }
+
+}
