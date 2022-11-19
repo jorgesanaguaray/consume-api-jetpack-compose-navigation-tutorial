abea2703857ee270069c068d6b4787303d6d74f5
Jorge Sanaguaray <jorgesanaguaray1@gmail.com>
2022-11-18 23:14:37 -0500

    I created the Constants class where I added variables that will be used later.

---
+++ app/src/main/java/com/jorgesanaguaray/consumeapijetpackcomposenavigationtutorial/util/Constants.kt
@@ -0,0 +1,26 @@
+package com.jorgesanaguaray.consumeapijetpackcomposenavigationtutorial.util
+
+/**
+ * Created by Jorge Sanaguaray
+ */
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://www.freetogame.com/api/"
+        const val GAMES_ENDPOINT = "games"
+        const val GAME_ID_ENDPOINT = "game?id="
+
+        const val KEY_GAME_ID = "com.jorgesanaguaray.practice6.id"
+
+    }
+
+    object Screens {
+
+        const val HOME_SCREEN = "home_screen"
+        const val DETAIL_SCREEN = "detail_screen"
+
+    }
+
+}
