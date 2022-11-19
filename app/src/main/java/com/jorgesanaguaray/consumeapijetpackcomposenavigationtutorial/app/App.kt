a987d5fc6c08ee5f98aeefce2492bd20503c634b
Jorge Sanaguaray <jorgesanaguaray1@gmail.com>
2022-11-18 23:09:17 -0500

    I created the App class and added it to the AndroidManifest, also added internet permission.

---
+++ app/src/main/java/com/jorgesanaguaray/consumeapijetpackcomposenavigationtutorial/app/App.kt
@@ -0,0 +1,11 @@
+package com.jorgesanaguaray.consumeapijetpackcomposenavigationtutorial.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Jorge Sanaguaray
+ */
+
+@HiltAndroidApp
+class App : Application()
