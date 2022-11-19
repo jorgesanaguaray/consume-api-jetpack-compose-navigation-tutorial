d0ac8d3ecfcc8ebe219bce74f674014541d70efb
Jorge Sanaguaray <jorgesanaguaray1@gmail.com>
2022-11-18 23:20:32 -0500

    I added the data classes obtained from the API.

---
+++ app/src/main/java/com/jorgesanaguaray/consumeapijetpackcomposenavigationtutorial/data/remote/models/GameModel.kt
@@ -0,0 +1,14 @@
+package com.jorgesanaguaray.consumeapijetpackcomposenavigationtutorial.data.remote.models
+
+/**
+ * Created by Jorge Sanaguaray
+ */
+
+data class GameModel(
+
+    val id: Int,
+    val title: String,
+    val thumbnail: String,
+    val short_description: String
+
+)
---
+++ app/src/main/java/com/jorgesanaguaray/consumeapijetpackcomposenavigationtutorial/data/remote/models/SpecificGameModel.kt
@@ -0,0 +1,21 @@
+package com.jorgesanaguaray.consumeapijetpackcomposenavigationtutorial.data.remote.models
+
+/**
+ * Created by Jorge Sanaguaray
+ */
+
+data class SpecificGameModel(
+
+    val id: Int,
+    val title: String,
+    val thumbnail: String,
+    val game_url: String,
+    val description: String,
+    val status: String,
+    val genre: String,
+    val platform: String,
+    val publisher: String,
+    val developer: String,
+    val release_date: String
+
+)
