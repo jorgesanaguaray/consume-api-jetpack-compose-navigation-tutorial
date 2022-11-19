9b356eed07d56e2afebc2cd2e5a439b169b291ac
Jorge Sanaguaray <jorgesanaguaray1@gmail.com>
2022-11-19 08:46:22 -0500

    I moved the MainActivity class and modified it so that it calls the function in charge of navigation.

--- app/src/main/java/com/jorgesanaguaray/consumeapijetpackcomposenavigationtutorial/ui/main/MainActivity.kt
+++ app/src/main/java/com/jorgesanaguaray/consumeapijetpackcomposenavigationtutorial/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.jorgesanaguaray.consumeapijetpackcomposenavigationtutorial
+package com.jorgesanaguaray.consumeapijetpackcomposenavigationtutorial.ui.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,38 +6,39 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import com.jorgesanaguaray.consumeapijetpackcomposenavigationtutorial.ui.nav.SetupNavHost
 import com.jorgesanaguaray.consumeapijetpackcomposenavigationtutorial.ui.theme.ConsumeApiJetpackComposeNavigationTutorialTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+
             ConsumeApiJetpackComposeNavigationTutorialTheme {
-                // A surface container using the 'background' color from the theme
+
                 Surface(
+
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
+
                 ) {
-                    Greeting("Android")
+
+                    val navController = rememberNavController()
+                    SetupNavHost(navController = navController, gameViewModel = viewModel())
+
                 }
+
             }
+
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ConsumeApiJetpackComposeNavigationTutorialTheme {
-        Greeting("Android")
     }
+
 }
