f7b50b8934dfdb8c07045371afe4fa97e2aa33ea
Jorge Sanaguaray <jorgesanaguaray1@gmail.com>
2022-11-21 08:54:31 -0500

    I deleted attributes of the data classes that were not being used.

--- app/src/main/java/com/jorgesanaguaray/consumeapijetpackcomposenavigationtutorial/data/remote/models/SpecificGameModel.kt
+++ app/src/main/java/com/jorgesanaguaray/consumeapijetpackcomposenavigationtutorial/data/remote/models/SpecificGameModel.kt
@@ -9,13 +9,6 @@ data class SpecificGameModel(
     val id: Int,
     val title: String,
     val thumbnail: String,
-    val game_url: String,
-    val description: String,
-    val status: String,
-    val genre: String,
-    val platform: String,
-    val publisher: String,
-    val developer: String,
-    val release_date: String
+    val description: String
 
 )
--- app/src/main/java/com/jorgesanaguaray/consumeapijetpackcomposenavigationtutorial/domain/items/SpecificGameItem.kt
+++ app/src/main/java/com/jorgesanaguaray/consumeapijetpackcomposenavigationtutorial/domain/items/SpecificGameItem.kt
@@ -11,15 +11,8 @@ data class SpecificGameItem(
     val id: Int,
     val title: String,
     val thumbnail: String,
-    val game_url: String,
-    val description: String,
-    val status: String,
-    val genre: String,
-    val platform: String,
-    val publisher: String,
-    val developer: String,
-    val release_date: String
+    val description: String
 
 )
 
-fun SpecificGameModel.toSpecificGameItem() = SpecificGameItem(id, title, thumbnail, game_url, description, status, genre, platform, publisher, developer, release_date)
+fun SpecificGameModel.toSpecificGameItem() = SpecificGameItem(id, title, thumbnail, description)
